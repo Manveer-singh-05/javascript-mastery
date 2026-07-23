bde77a6e71dfd48fb90966fc1b8139c25df58832
Manver Singh <manveersingh0501l@gmail.com>
2026-07-23 22:20:25 +0530

    added datatypes

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+// there are different types of data types to store different different data
+//1. string which stores the name
+let name = "Manveer";
+console.log(name);
+
+
+//2. number which stores the integer types values here u can see that it can store both integer and decimal values
+let num = 21123;
+let nu = 33.2;
+console.log(num);
+console.log(nu);
+
+//3. boolean which stores only true or false
+let flag = true;
+console.log(flag);
+
+//4. undefined it is a data types that have not assign any value and it also not null
+let x ;
+console.log(x); //in terminal its shows undefined 
+
+//5. Null different from undefined it means there  is nothing there means emptyness
+let man = null;
+console.log(man);
+
+//6. BigInt - it is used when normal number datatypes cant handle large amount of int so we use it to define the big int we have to use n at the end of the number 
+let m = 12345678902134567890n;
+console.log(m);
+
+//
