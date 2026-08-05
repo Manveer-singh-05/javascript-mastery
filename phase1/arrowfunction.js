bebc5ffb0d3dcb60bd38012f569d5e3fa50d90b6
Manver Singh <manveersingh0501l@gmail.com>
2026-08-05 22:45:26 +0530

    Added arrow function

---
+++ phase1/arrowfunction.js
@@ -0,0 +1,12 @@
+// arrow function is nothing just a shorter syntax and easy syntax to write a function
+
+const greet  = (name) =>{
+    return "hello " + name;
+}
+console.log(greet("manveer"));
+
+// adding two values
+const add = (a,b) =>{
+    return a+b;
+}
+console.log(add(2,3));
