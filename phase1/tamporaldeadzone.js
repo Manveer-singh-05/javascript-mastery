bf47933aa4c4b617213f43e645e7ee1e4b206d6d
Manver Singh <manveersingh0501l@gmail.com>
2026-09-10 21:51:10 +0530

    added tamporal deadzone

---
+++ phase1/tamporaldeadzone.js
@@ -0,0 +1,7 @@
+console.log(k);
+console.log("hey  there");
+console.log("hello");
+let k = 10;
+
+//Temporal Dead Zone is the time period between entering a scope and the point where a let or const variable is initialized. During this period, accessing the variable results in a ReferenceError.
+// in this given example from line 1 to 4 there will be temporal deadzone
