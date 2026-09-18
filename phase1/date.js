1ba26b7e7d4d8145cd3cad21c58159fcf36abc06
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 22:34:18 +0530

    added date

---
+++ phase1/date.js
@@ -0,0 +1,16 @@
+let curr = new Date();
+
+let data = new Date('June 20 2000 7:39');
+
+let newdate= new Date(2005 ,11, 21, 4);
+console.log(curr);
+console.log(data);
+console.log(newdate);
+
+
+console.log(newdate.getFullYear());
+
+newdate.setFullYear(2001);
+console.log(newdate);
+
+
