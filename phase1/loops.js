b862989a46d76260a159223ff1f4ad57b1521c49
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 18:16:11 +0530

    loops

---
+++ phase1/loops.js
@@ -0,0 +1,19 @@
+// for loop used where we know the number of iteration
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+
+// while loop used where we dont know the number of iterations 
+let i = 5;
+while(i<8){
+    console.log(i);
+    i++;
+}
+
+
+// do while loop is the loop that guranteed one iteration even though the conditon is not satisifed
+let m = 2;
+do{
+    console.log(m);
+    m++;
+}while(m<2);
