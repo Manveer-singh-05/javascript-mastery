53978ee5b5a32743e8adbdacf13e1ad82ae3ba26
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 18:44:39 +0530

    added primitve and non primitive

---
+++ phase1/primitiveandnonprimitive.js
@@ -0,0 +1,8 @@
+// primitive data is the data types that contains only single value ex boolean, int, string and the data shared in primitve data type is by pass by value
+// non primitive data type that contains the collections of valsue like object , array, function and the data shared in the non primitive data type is pass by reference
+let a = 10;
+console.log(a);
+
+let arr = [12,44];
+console.log(arr);
+
