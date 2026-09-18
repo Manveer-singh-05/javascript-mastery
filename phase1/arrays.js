793db35a0522798f8d5fcdcea94c97b24ed56fb9
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 19:14:19 +0530

    Added arrays.cpp

--- phase1/arrays.js
+++ phase1/arrays.js
@@ -12,11 +12,26 @@ arr.unshift(22);
 // removing first element
 arr.shift();
 
+
+// slice method  extract the specific part
+let k = [12,35,566, 335]
+let op=  k.slice(1,2);
+console.log(k);
+console.log(op);
+
+// splice = content change in array like insert, remove and update
+
+k.splice(1,3,"manveer");  // splice(start, no of removal, inserting values);
+console.log(k);
+
+
+
 let n = arr.length;
 for(let i=0;i<n;i++){
     console.log(arr[i]);
 }
 console.log(arr);
+
 
 
 // using some logic or searching some thing
