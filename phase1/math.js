a03eb670e00b852d381972e4c78ed95ec11a4a48
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 22:25:21 +0530

    added math.js

---
+++ phase1/math.js
@@ -0,0 +1,19 @@
+console.log(Math.PI);
+
+console.log(Math.max(1,4,2,5,55));
+
+console.log(Math.min(2,4,3,5));
+
+console.log(Math.round(2.67));
+
+console.log(Math.floor(2.5));  // give the lowest near value
+
+console.log(Math.ceil(4.1));    // give the higher near value
+
+console.log(Math.abs(-1))  // give positive vlaue
+
+console.log(Math.random());
+
+console.log(Math.pow(2,4));
+
+console.log(Math.sqrt(4));
