7506abeec34216d09c4f1029f77c0541db2a67f5
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 23:14:33 +0530

    added some changes

--- phase1/errorhandling.js
+++ phase1/errorhandling.js
@@ -15,3 +15,5 @@ finally{
     console.log("i will run evey time as i am a finally block")
 }
 
+
+// why do we need finally ->  The finally keyword provides a guarantee that a specific block of code will always execute, regardless of whether an exception is thrown, caught, or completely unhandled
