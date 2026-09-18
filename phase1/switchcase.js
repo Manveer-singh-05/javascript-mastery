97b03bd0f77569c18ee7972f73384c3cd0b68933
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 18:10:54 +0530

    added switch case

---
+++ phase1/switchcase.js
@@ -0,0 +1,17 @@
+// switch is used when there are multiple case can happen
+let num =3;
+switch(num){
+    case 1:
+        console.log("1");
+        break;
+    case 2:
+        console.log("2");
+        break;
+    case 3:
+        console.log("3");
+        break;
+    default:
+        console.log("defualt");
+}   
+
+// there is a specila case like if we forget to use break in case and the case passed then after that passed case the remaininig case also printed
