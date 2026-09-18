00fbb39a35eecec5bee717ccab24db1022cb6646
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 23:14:14 +0530

    added error handling

---
+++ phase1/errorhandling.js
@@ -0,0 +1,17 @@
+try{
+
+    console.log("manveer");
+    console.log(x);
+}
+catch(err){
+
+    // how can we declare custom error using throw keyword
+    throw new Error("bhai pehle declare toh kare le ")
+    // console.log("i get an error");
+    
+}
+
+finally{
+    console.log("i will run evey time as i am a finally block")
+}
+
