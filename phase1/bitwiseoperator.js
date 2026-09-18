38b7ed6dc81d1bc1f4b2381a189bc2b06293b0ce
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 18:03:38 +0530

    added left and right shift and not shif

---
+++ phase1/bitwiseoperator.js
@@ -0,0 +1,15 @@
+//bitwise not (~)  it menas flipping all the bits at bit level 
+
+// most imp concpet that is ~(0) = -1
+
+console.log(~(0));
+
+
+// left shift just shifts the bits at left and fill the empty place with zeros
+//shortcut trick is to calculate it (number multiply by 2 ki power shift) for example 10<<1 that means (10* (2**1))
+console.log(10<<1);
+
+
+// right shift just shift the bits at right and remove that right digit by given shift 
+// shortcut trick is to calculate it (number divide by 2 ki power shift) for example 10>>1 that means (10/ (2**1));
+console.log(10>>1);
