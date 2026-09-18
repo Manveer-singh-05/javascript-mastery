371ad221ae49f5c64ff8d72175a00351caa06d5d
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 17:36:42 +0530

    added loose and strict equality

---
+++ phase1/looseandstrictequality.js
@@ -0,0 +1,7 @@
+// strict equality means === in this value and datatype must be same 
+console.log(5===5) // true;
+console.log(5==='5') //false;
+
+// loose equality means == in this only value compares not data type
+console.log(5==5); //true
+console.log(5=='5') //true
