871c046b14961b5f976e87b08dd5d5b7a9a3f2d1
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 17:43:13 +0530

    added falsy and truthy

---
+++ phase1/falsyandtruthy.js
@@ -0,0 +1,17 @@
+// falsy and truthy is concept in which we are working with  non boolean variables like ture && 3
+
+//so under falsy 
+/*
+1. undefined
+2. null
+3. nan
+4. 0
+5. false
+6. ' '  empty string
+*/
+
+// truthy = anything that is not under falsy is under truthy
+
+if(true && 4){
+    console.log("hey");
+}
