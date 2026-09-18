6fbb6c443099ced9d2fde0a474ea656caf0c5785
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 17:25:09 +0530

    added letvs var.js

---
+++ phase1/letvsvar.js
@@ -0,0 +1,7 @@
+let x=  12; // can do redeclare with the same name and it is block scope used widely
+ x = "manveer";
+console.log(x);
+
+var i  = 34; // can do redecalre with the same name but we have to avoid it for better debugging and it is function scoped avoid using it 
+var i = 33;
+console.log(i);
