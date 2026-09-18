a98d001e962e1508c5c4b8cdfd8bc540f534b24c
Manver Singh <manveersingh0501l@gmail.com>
2026-09-18 21:36:49 +0530

    added loops

--- phase1/loops.js
+++ phase1/loops.js
@@ -17,3 +17,27 @@ do{
     console.log(m);
     m++;
 }while(m<2);
+
+
+// for each loop
+let arr = [12,24,66,77,3233];
+let ansarray = arr.forEach((value)=>{
+    console.log(value);
+})
+
+// for in function
+let ob = {
+    name: "manveer singh",
+    age: 21,
+    class : "btech"
+}
+for(let keys in ob){
+    console.log(keys, ob[keys]);
+}
+
+
+// for of loop
+let newa = [3,4,5,8,3,7];
+for(let x of newa){
+    console.log(x);
+}
